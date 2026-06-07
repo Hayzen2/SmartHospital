6a0b41f58254926348c7db8f6f4b82a06196489b
Hayzen2 <baongocngo2004@gmail.com>
2026-06-07 13:23:18 +0700

    Disable swagger for production

--- src/main/java/com/example/SmartHospital/config/MinioConfig.java
+++ src/main/java/com/example/SmartHospital/config/MinioConfig.java
@@ -1,14 +1,15 @@
 package com.example.SmartHospital.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MinioConfig {
@@ -44,6 +45,7 @@ public class MinioConfig {
                 .withPathStyleAccessEnabled(true) 
                 .withClientConfiguration(clientConfig)
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withPathStyleAccessEnabled(true) // Ensure path-style access is enabled for Minio
                 .disableChunkedEncoding() 
                 .build();
     }
